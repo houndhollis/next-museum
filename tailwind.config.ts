36564717d07cdf3391dbb6ffd2a35c336eca32a3
houndhollis <houndhollis9797@gmail.com>
2024-10-21 22:52:29 +0900

    [🎉 REFACTOR] : 태일윈드 설정 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+import { color } from "./colorPalette";
+
+const { createThemes } = require("tw-colors");
 
 const config: Config = {
   content: [
@@ -7,13 +10,50 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      inherit: "inherit",
+      xs: "1.2rem",
+      s: "1.4rem",
+      m: "1.6rem",
+      l: "1.8rem",
+      xl: "2.0rem",
+      "2xl": "2.2rem",
+      "3xl": "2.4rem",
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      spacing: {
+        xs: "1.2rem",
+        s: "1.4rem",
+        m: "1.6rem",
+        l: "1.8rem",
+        xl: "2.0rem",
+        "2xl": "2.2rem",
+        "3xl": "2.4rem",
+      },
+      borderRadius: {
+        4: "4px",
+        8: "8px",
+        12: "12px",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    createThemes({
+      light: {
+        primary: color.blue[700],
+        secondary: color.blue[900],
+        accent: color.blue[300],
+      },
+      dark: {
+        primary: color.blue[200],
+        secondary: color.blue[400],
+        accent: color.blue[0],
+      },
+    }),
+  ],
 };
 export default config;
